f0a0442d72c68df1677732505ccb8fe33c3cc8b9
seaamm <98990502+seaamm@users.noreply.github.com>
2022-03-03 21:04:43 +1300

    Update Raffle.py

--- Raffle.py
+++ Raffle.py
@@ -1,206 +1,37 @@
-Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
+'''Raffle Program'''
+import random
 
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
+print ("Welcome to the raffle Program")
 
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-ty
-hi
-end
-
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-owen
-dixel
-flynn
-sea-am
-end
-['owen', 'dixel', 'flynn', 'sea-am']
-
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-Welcome to the raffle program
-owen
-dixel
-flynn
-sea-am
-end
-There are 4 people in the draw for the surfboard
-Traceback (most recent call last):
-  File "/Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py", line 18, in <module>
-    print (f"And the winner of the {prize}, valued at {price}, is...... {winner}")
-NameError: name 'prize' is not defined. Did you mean: 'print'?
-
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-Welcome to the raffle program
-Whiat is the prize being raffled?
-
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-Welcome to the raffle program
-What is the prize being raffled?
-l
-What is the value of the l (do not enter the $ sign)
-0
+print ("What is the prize being raffled?")
+try:
+  prize = input()
+except ValueError:
+  print ("Please enter a valid prize")
 
 
+print (f"What is the value of the {prize} (do not enter the $ sign)")
+try:
+  value = int(input())
+except ValueError:
+  print ("Please enter a valid value")
 
+raffle = True
 
+name_list = []
 
+while raffle == True:
+    name = input("Enter name of entrant: ")
+    
+  
+    if name.lower() == "end":
+        raffle = False
+    else:
+        name_list.append(name)
 
+number = random.randint(0, (len(name_list)))
+winner = name_list[number]
 
+print (f"There are {len(name_list)} people in the draw for the {prize}")
 
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-Welcome to the raffle program
-What is the prize being raffled?
-l
-What is the value of the l (do not enter the $ sign)
-;
-Traceback (most recent call last):
-  File "/Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py", line 11, in <module>
-    value = int(input())
-ValueError: invalid literal for int() with base 10: ';'
-
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-Welcome to the raffle program
-What is the prize being raffled?
-Flynn
-What is the value of the Flynn (do not enter the $ sign)
-0
-Enter names here:
-Owen
-Dixel
-Sea-am
-Flynn
-end
-There are 4 people in the draw for the surfboard
-And the winner of the Flynn, valued at 0, is...... 1
-
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-Welcome to the raffle program
-What is the prize being raffled?
-0
-What is the value of the 0 (do not enter the $ sign)
-0
-Enter names here:
-on3
-de3
-2d
-2s
-end
-There are 4 people in the draw for the surfboard
-And the winner of the 0, valued at 0, is...... 3
-
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-Welcome to the raffle program
-What is the prize being raffled?
-1
-What is the value of the 1 (do not enter the $ sign)
-1
-Enter names here:
-1
-2
-3
-4
-end
-Traceback (most recent call last):
-  File "/Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py", line 25, in <module>
-    print (name_list(number))
-TypeError: 'list' object is not callable
-
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-Welcome to the raffle program
-What is the prize being raffled?
-1
-What is the value of the 1 (do not enter the $ sign)
-1
-Enter names here:
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-end
-Traceback (most recent call last):
-  File "/Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py", line 25, in <module>
-    print ([name_list(number)])
-TypeError: 'list' object is not callable
-
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-Welcome to the raffle program
-What is the prize being raffled?
-111
-What is the value of the 111 (do not enter the $ sign)
-123
-Enter names here:
-
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-Welcome to the raffle program
-What is the prize being raffled?
-111
-What is the value of the 111 (do not enter the $ sign)
-123
-Enter names here:
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-end
-6
-There are 10 people in the draw for the surfboard
-Traceback (most recent call last):
-  File "/Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py", line 32, in <module>
-    print (f"And the winner of the {prize}, valued at {value}, is...... {winner}")
-NameError: name 'winner' is not defined
-
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-Welcome to the raffle program
-What is the prize being raffled?
-Owen's Computer
-What is the value of the Owen's Computer (do not enter the $ sign)
-1000
-Enter names here:
-Owen
-Dixel
-Flynn
-Sea-am
-end
-There are 4 people in the draw for the surfboard
-And the winner of the Owen's Computer, valued at 1000, is...... Flynn
-potato
-Traceback (most recent call last):
-  File "<pyshell#0>", line 1, in <module>
-    potato
-NameError: name 'potato' is not defined
-y
-Traceback (most recent call last):
-  File "<pyshell#1>", line 1, in <module>
-    y
-NameError: name 'y' is not defined
-
-= RESTART: /Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py
-Welcome to the raffle program
-What is the prize being raffled?
-my computer, aka a liability
-What is the value of the my computer, aka a liability (do not enter the $ sign)
--1000000
-Enter names here:
-flynn
-owen
-sea-am
-dixel
-end
-Traceback (most recent call last):
-  File "/Users/seaam/Documents/OneDrive - St Andrews College/2022/Digital Technology/2022-year-11-python-classwork-seaamm/Raffle.py", line 29, in <module>
-    winner = (name_list[number])
-IndexError: list index out of range
+print (f"And the winner of the {prize}, valued at ${value}, is...... {winner}")
